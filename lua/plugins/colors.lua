eb78b792decf96d293c1618135fe07b877be4828
calebcav <cavinesscaleb@gmail.com>
2025-12-16 23:17:56 -0600

    changed theme back to rose pine

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,30 +1,18 @@
-local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-end
+-- local function enable_transparency()
+--     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- end
 
 return {
-    -- {
-    --     "folke/tokyonight.nvim",
-    --     config = function()
-    --         vim.cmd.colorscheme("tokyonight")
-    --         -- enable_transparency()
-    --     end,
-    -- },
-    -- lua/plugins/rose-pine.lua
     {
-        "neanias/everforest-nvim",
-        name = "everforest-light-med",
+        "rose-pine/neovim",
+        name = "rose-pine-moon",
         config = function()
-            require("everforest").setup({
-                background = "medium",
-            })
-            vim.o.background = "light"
-            vim.cmd.colorscheme("everforest")
+            vim.cmd.colorscheme("rose-pine-moon")
         end,
     },
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
-        opts = { theme = "everforest" },
+        opts = { theme = "rose-pine-moon" },
     },
 }
--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -1,17 +1,17 @@
 return {
-    {
-        "tpope/vim-fugitive",
-    },
-    { "windwp/nvim-autopairs", event = "InsertEnter", config = true },
-    { "wakatime/vim-wakatime", lazy = false },
-    {
-        "sphamba/smear-cursor.nvim",
-        opts = {
-            cursor_color = "#E69875",
-            stiffness = 0.5,
-            trailing_stiffness = 0.5,
-            matrix_pixel_threshold = 0.5,
-        },
-    },
-    { "dmmulroy/ts-error-translator.nvim" },
+	{
+		"tpope/vim-fugitive",
+	},
+	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
+	{ "wakatime/vim-wakatime", lazy = false },
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {
+			cursor_color = "#FFFFFF",
+			stiffness = 0.5,
+			trailing_stiffness = 0.5,
+			matrix_pixel_threshold = 0.5,
+		},
+	},
+	{ "dmmulroy/ts-error-translator.nvim" },
 }
